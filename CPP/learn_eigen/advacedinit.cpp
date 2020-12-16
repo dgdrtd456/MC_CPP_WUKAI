2a549f7ea4bdfef962832a140654fb1a3f20df30
dgdrtd456 <641179433@qq.com>
2020-12-16 09:21:31 +0800

    master

---
+++ CPP/learn_eigen/advacedinit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "eigen-3.3.9/Eigen/Dense"
+#include "eigen-3.3.9/Eigen/
+
+using namespace Eigen;
+using namespace std;
+
+int main()
+{
+  MatrixXd m = MatrixXd::Random(3,3);
+  m = (m + MatrixXd::Constant(3,3,1.2)) * 50;
+  cout << "m =" << endl << m << endl;
+  VectorXd v(3);
+  v << 1, 2, 3;
+  cout << "m * v =" << endl << m*v << endl;
+}
